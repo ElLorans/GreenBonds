dac5f06a6dac9a0efdf14843f3a4f7ccd8b5df20
ElLorans <lorenzo.cerreta@libero.it>
2020-09-17 18:10:21 +0200

    Add files via upload

---
+++ utilities/columns.py
@@ -0,0 +1,5 @@
+col_names = {'Issuer', 'Ticker', 'Coupon', 'Maturity', 'Issue Date', 'ISIN',
+       'Preferred RIC', 'Principal Currency', 'Country of Issue',
+       'Issuer Type', 'Instrument Type', 'Coupon Type', 'Amount Issued (USD)',
+       'Bond Grade', 'Green Bond', 'Sector', 'TRBC Sector',
+       'Country of Incorporation', 'SIC', 'Use of Proceeds'}
